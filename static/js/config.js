354ca794b355a45358207986f31470378917a756
Erick Aguilar <auge_1985@hotmail.com>
2026-09-14 13:48:18 -0600

    fix(config): corregir bytecode de validacion WebAssembly SIMD128 para evitar falso negativo

--- static/js/config.js
+++ static/js/config.js
@@ -128,7 +128,7 @@
       var hasSimd = false;
       try {
         hasSimd = typeof WebAssembly !== 'undefined' && WebAssembly.validate(
-          new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 5, 1, 96, 0, 1, 123, 3, 2, 1, 0, 10, 10, 1, 8, 0, 65, 0, 253, 15, 26, 11])
+          new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 5, 1, 96, 0, 1, 123, 3, 2, 1, 0, 10, 8, 1, 6, 0, 65, 0, 253, 15, 11])
         );
       } catch (e) {
         hasSimd = false;
